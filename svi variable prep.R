d59a27d84bbdd0fbae543c995f8ffc3fe4c83c30
Heli Xu <xuheli91@gmail.com>
2022-11-22 18:33:54 -0500

    include adjunct variables in census pull and save calculation in a table
    filter from the table for generating svi variables

--- svi variable prep.R
+++ svi variable prep.R
@@ -47,72 +47,51 @@ var_cal <- var_cal %>%
 #but for E_ and EP_,calculation is complete and matching variable
 #so you could safely filter out x when needed
 
+#subsetting E_ and EP_ variables
+var_prefix <- c("E_", "EP_")
+
+var_cal2 <- 
+  var_cal %>%
+  filter(str_detect(x2020_variable_name, paste(var_prefix, collapse = "|"))) %>% 
+  mutate(theme = case_when(
+    x2020_variable_name%in%c("E_TOTPOP","E_HU","E_HH") ~ 0,  #must be same class
+    is.na(theme) ~ 5, #adjunct variables
+    TRUE ~ theme
+  ))
+
+#save a simpler table to load into calculation (name, theme, cal)
+var_cal_eep <- var_cal2 %>% 
+  select(-census_var)
+
+saveRDS(var_cal_eep, file = "data/variable_e_ep_calculation.rds")
 
 
 #make a function to pull variables from each theme 
 #E_ and EP_ (estimate and percentage of total, which *sometimes pull new variables)
 theme_var_df <- function(n){
-  var_prefix <- c("E_", "EP_")
-  
-  var_cal %>% 
-    filter(theme == n,
-           str_detect(x2020_variable_name, paste(var_prefix, collapse = "|"))) %>%
+  var_cal2 %>% 
+    filter(theme == n) %>%
     select(x2020_variable_name, census_var) %>% 
     separate_rows(census_var, sep = " ")  %>% 
     filter(!str_starts(census_var, "E_"),
-           !census_var%in%c("","100"))
+           !census_var%in%c("","100")) %>% 
+    pull(census_var)
 }
 
-theme1_var <- theme_var_df(1) %>% 
-  pull(census_var)  #inspect before pull
 
-theme2_var <- theme_var_df(2) %>% 
-  pull(census_var)
 
-theme3_var <- theme_var_df(3) %>% 
-  pull(census_var)
+var_list <- map(0:5, theme_var_df) 
 
-theme4_var <- theme_var_df(4) %>% 
-  pull(census_var)
-
-#first 3 params used as denominators
-theme_xtr <- var_cal %>% 
-  filter(x2020_variable_name%in%c("E_TOTPOP","E_HU","E_HH")) %>% 
-  pull(census_var)
-  
-var_list <- list(t1 = theme1_var, 
-                 t2 = theme2_var, 
-                 t3 = theme3_var, 
-                 t4 = theme4_var, 
-                 tx = theme_xtr) 
+#name elements in the list by theme (t0 = total, t5 = adjunct)
+names(var_list) <- c("t0","t1","t2","t3","t4","t5")
 
 saveRDS(var_list, "data/census_variables.rds")  
 
 
-#construct simpler table for calculation
-var_prefix <- c("E_", "EP_")
-
-var_cal2 <- 
-  var_cal %>%
-  select(-census_var) %>% 
-  filter(str_detect(x2020_variable_name, paste(var_prefix, collapse = "|"))) %>% 
-  mutate(theme = case_when(
-           x2020_variable_name%in%c("E_TOTPOP","E_HU","E_HH") ~ 0,  #must be same class
-           is.na(theme) ~ 5, #adjust variables
-           TRUE ~ theme
-         ))
 
 #make a named vector storing calculation
-var_name <- var_cal2$x2020_variable_name
-
-var_expr <- var_cal2$x2020_table_field_calculation
-
-names(var_expr) <- var_name
-
-
-var_expr["EP_ASIAN"]  
-
-saveRDS(var_expr, file = "data/variable_calculation.rds")
+#this has been incorporated into svi_calculation 
+#(E_ and EP_ have to be processed seperately, in order)
 
 
 # construct a xwalk for theme and variable
