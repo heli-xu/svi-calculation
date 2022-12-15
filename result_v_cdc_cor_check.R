3c3ed497fefa183e15e17cb381afa5e55fff3ee3
Heli Xu <xuheli91@gmail.com>
2022-12-15 17:20:35 -0500

    update checking correlation for individual theme

--- result_v_cdc_cor_check.R
+++ result_v_cdc_cor_check.R
@@ -1,16 +1,33 @@
+library(skimr)
+
 ## load cdc data
-RI_2014_ct <- read_csv("download/RI_tract_2014.csv") %>% 
+PA_2016_svi_co <- read_csv("download/2016svi_pa_co_cdc.csv") %>% 
   rename(GEOID = FIPS) 
 
 ## joining data with our result
-dfa = RI_2014_ct %>% 
+dfa <- PA_2016_svi_co %>% 
   select(
-    GEOID, res_cdc = RPL_THEMES) %>% 
+    GEOID, 
+    cdc_RPL_themes = RPL_THEMES, 
+    cdc_RPL_theme1 = RPL_THEME1,
+    cdc_RPL_theme2 = RPL_THEME2,
+    cdc_RPL_theme3 = RPL_THEME3,
+    cdc_RPL_theme4 = RPL_THEME4
+    ) %>% 
   mutate(GEOID = paste(GEOID)) %>% 
   left_join(
     svi_complete %>% 
-      select(GEOID, res_heli = RPL_themes)
+      select(
+        GEOID, 
+        RPL_themes,
+        RPL_theme1,
+        RPL_theme2,
+        RPL_theme3, 
+        RPL_theme4
+        )
   ) 
+
+
 
 #check NA (sometimes CDC has -999 for NA)
 skim(dfa)
@@ -23,9 +40,14 @@ dfa <- dfa %>%
   drop_na(res_heli)
 
 ## check correlation between cdc result and ours
-cor(dfa$res_cdc, dfa$res_heli)
+cor(dfa$cdc_RPL_themes, dfa$RPL_themes)
+cor(dfa$cdc_RPL_theme1, dfa$RPL_theme1)
+cor(dfa$cdc_RPL_theme2, dfa$RPL_theme2)
+cor(dfa$cdc_RPL_theme3, dfa$RPL_theme3)
+cor(dfa$cdc_RPL_theme4, dfa$RPL_theme4)
+
 
 dfa %>% 
-  ggplot(aes(x = res_cdc, y = res_heli)) +
+  ggplot(aes(x = cdc_RPL_theme2, y = RPL_theme2)) +
   geom_point()+
   geom_abline(slope = 1, intercept = 0, color = 'red')
