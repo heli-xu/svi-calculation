33b9998b160a3c3ba4d55d11a6b18b4a7485a765
Heli Xu <xuheli91@gmail.com>
2023-01-17 22:41:43 -0500

    weighted mean RPL_theme(s); aggregate E_values (sum)
    weighted mean RPL_themes not working; E_value sum correlated ok; to do: EP_values needs aggregated by mean

--- aggregate_ct_to_zcta_cdc_svi.R
+++ aggregate_ct_to_zcta_cdc_svi.R
@@ -6,42 +6,76 @@ ct_zcta <- read.table("cdc_us_svi/tab20_zcta520_tract20_natl.txt",
 
 ct_zcta_xwalk2020 <- ct_zcta %>% 
   select(ZCTA = GEOID_ZCTA5_20, 
-    GEOID = GEOID_TRACT_20) 
+    GEOID = GEOID_TRACT_20) %>% 
+  mutate(ZCTA = as.character(ZCTA))
 
-ct_zcta %>% group_by(GEOID_ZCTA5_20) %>% 
-  drop_na(GEOID_ZCTA5_20) %>% 
-  count()
+pa_c
 
-skim(ct_zcta)  
+#load population in each zcta (or pull again S0601_C01_001E)
+
+# weighted by population
+pop2020_zcta <- data2020_pa %>%  
+  select(GEOID, "S0601_C01_001E")
+
+ct_zcta_xwalk2020<- ct_zcta_xwalk2020 %>% 
+  left_join( pop2020_zcta %>% rename(ZCTA = GEOID), 
+    by = "ZCTA") %>% 
+  mutate(ZCTA_POP = S0601_C01_001E)
+
 
 svi_pa_2020 <- read_csv("cdc_us_svi/cdc_svi_2020_pa_ct.csv") %>% 
   rename(GEOID = FIPS)
 
 zsvi_pa_2020 <- svi_pa_2020 %>% 
-  left_join(ct_zcta_xwalk2020, by = "GEOID") 
+  left_join(ct_zcta_xwalk2020, by = "GEOID") %>% 
+  relocate(ZCTA, .after = GEOID)
+
+var_name2020 <- readRDS("data/variable_e_ep_calculation_2020.rds") %>% 
+  filter(theme%in%c(0:4)) %>% 
+  pull(1)
+
+cdc <- zsvi_pa_2020 %>% 
+  select(GEOID, ZCTA, all_of(var_name2020)) %>%
+  pivot_longer(-c(GEOID,ZCTA), names_to = "var_name", values_to = "value") %>% 
+  group_by(ZCTA, var_name) %>% 
+  summarise(var_zcta = sum(value))
+
+result <- result2020_2 %>% 
+  select(ZCTA = GEOID, all_of(var_name2020)) %>% 
+  pivot_longer(-ZCTA, names_to = "var_name", values_to = "value_hx") 
+
+cdc_result <- cdc %>% 
+  left_join(result, by= c("ZCTA", "var_name")) %>% 
+  drop_na() %>% 
+  group_by(var_name) %>% 
+  summarise(cor = cor(var_zcta, value)) %>% 
+  ungroup()
 
 skim(zsvi_pa_2020)
 # check ZCTA NAs, worked BUT too many columns, pretty crazy report
 # better to just filter:
 zsvi_pa_2020 %>% filter(is.na(ZCTA))
 
+zsvi_pa_2020 %>% 
+  select(GEOID, ZCTA, E_TOTPOP, S0601_C01_001E)
+
 zzsvi_pa_2020 <- zsvi_pa_2020 %>% 
   drop_na(ZCTA) %>% 
   filter_all(all_vars(.>=0)) %>% 
   group_by(ZCTA) %>% 
   summarise(
-    cdc_RPL_themes = mean(RPL_THEMES), 
-    cdc_RPL_theme1 = mean(RPL_THEME1),
-    cdc_RPL_theme2 = mean(RPL_THEME2),
-    cdc_RPL_theme3 = mean(RPL_THEME3),
-    cdc_RPL_theme4 = mean(RPL_THEME4)) %>% 
+    cdc_RPL_themes = weighted.mean(RPL_THEMES, w = AREA_SQMI), 
+    cdc_RPL_theme1 = weighted.mean(RPL_THEME1, w = AREA_SQMI),
+    cdc_RPL_theme2 = weighted.mean(RPL_THEME2, w = AREA_SQMI),
+    cdc_RPL_theme3 = weighted.mean(RPL_THEME3, w = AREA_SQMI),
+    cdc_RPL_theme4 = weighted.mean(RPL_THEME4, w = AREA_SQMI)) %>% 
   rename(GEOID = ZCTA) %>% 
   mutate(GEOID = paste(GEOID)) %>% 
   ungroup()
 
 dfa <- zzsvi_pa_2020 %>% 
   left_join(
-    result2020 %>% 
+   x <- result2020 %>% 
       select(
         GEOID, 
         RPL_themes,
