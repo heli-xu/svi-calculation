44cdfab6093ef00776c69801b6402a19d102a8a2
Heli Xu <xuheli91@gmail.com>
2023-01-20 17:37:22 -0500

    remove ct:zcta >1 from aggregation , check corr for 2020
    cdc data ct levels, some ct has multiple zctas, remove them to check corelation with calculated zcta svi. (aggregate method: sum e_, mean_ep/rpl).  Need to include to qmd

--- aggregate_ct_to_zcta_cdc_svi.R
+++ aggregate_ct_to_zcta_cdc_svi.R
@@ -30,59 +30,90 @@ zsvi_pa_2020 <- svi_pa_2020 %>%
   left_join(ct_zcta_xwalk2020, by = "GEOID") %>% 
   relocate(ZCTA, .after = GEOID)
 
-var_name2020 <- readRDS("data/variable_e_ep_calculation_2020.rds") %>% 
-  filter(theme%in%c(0:4)) %>% 
+zsvi_pa_2020 %>% filter(is.na(ZCTA))
+
+#when one census tract includes multiple zcta code, aggregated data usually quite off 
+#since you're doing sum for var, and you're adding bigger area to rep a small area
+
+ct_zcta_1_1 <- zsvi_pa_2020 %>% group_by(GEOID) %>% 
+  count() %>% 
+  arrange(n) %>% 
+  filter(n==1) %>% 
+  pull(GEOID)
+
+var_table <- readRDS("data/variable_e_ep_calculation_2020.rds")
+
+var_e <- var_table %>% 
+  filter(theme%in%c(0:4),
+    str_detect(.[[1]], "E_")) %>% 
+  pull(1)
+
+var_ep <- var_table %>% 
+  filter(theme%in%c(0:4),
+    str_detect(.[[1]], "EP_")) %>% 
   pull(1)
 
 cdc <- zsvi_pa_2020 %>% 
-  select(GEOID, ZCTA, all_of(var_name2020)) %>%
+  select(GEOID, ZCTA, all_of(var_e), all_of(var_ep)) %>%
+  filter(GEOID%in%all_of(ct_zcta_1_1)) %>% 
   pivot_longer(-c(GEOID,ZCTA), names_to = "var_name", values_to = "value") %>% 
+  filter(value >= 0) %>% 
   group_by(ZCTA, var_name) %>% 
-  summarise(var_zcta = sum(value))
+  # summarise(var_zcta = sum(value)) %>% 
+  summarise(sum = sum(value),
+    mean = mean(value)) %>%
+  mutate(var_zcta = case_when(
+    str_starts(var_name, "E_") ~ sum,
+    str_starts(var_name, "EP_") ~ mean
+  )) %>%
+  ungroup() %>% 
+  select(-sum, -mean)
+
 
 result <- result2020_2 %>% 
-  select(ZCTA = GEOID, all_of(var_name2020)) %>% 
-  pivot_longer(-ZCTA, names_to = "var_name", values_to = "value_hx") 
+  select(ZCTA = GEOID, all_of(var_e), all_of(var_ep)) %>% 
+  pivot_longer(-ZCTA, names_to = "var_name", values_to = "value_hx") %>% 
+  filter(value_hx >= 0)
 
 cdc_result <- cdc %>% 
   left_join(result, by= c("ZCTA", "var_name")) %>% 
   drop_na() %>% 
   group_by(var_name) %>% 
-  summarise(cor = cor(var_zcta, value)) %>% 
+  mutate(cor = cor(var_zcta, value_hx)) %>% 
   ungroup()
 
-skim(zsvi_pa_2020)
-# check ZCTA NAs, worked BUT too many columns, pretty crazy report
-# better to just filter:
-zsvi_pa_2020 %>% filter(is.na(ZCTA))
 
-zsvi_pa_2020 %>% 
-  select(GEOID, ZCTA, E_TOTPOP, S0601_C01_001E)
+##averaging ct RPLs to zcta levels
 
-zzsvi_pa_2020 <- zsvi_pa_2020 %>% 
+cdc2 <- zsvi_pa_2020 %>% 
   drop_na(ZCTA) %>% 
   filter_all(all_vars(.>=0)) %>% 
+  filter(GEOID%in%all_of(ct_zcta_1_1)) %>% 
   group_by(ZCTA) %>% 
   summarise(
-    cdc_RPL_themes = weighted.mean(RPL_THEMES, w = AREA_SQMI), 
-    cdc_RPL_theme1 = weighted.mean(RPL_THEME1, w = AREA_SQMI),
-    cdc_RPL_theme2 = weighted.mean(RPL_THEME2, w = AREA_SQMI),
-    cdc_RPL_theme3 = weighted.mean(RPL_THEME3, w = AREA_SQMI),
-    cdc_RPL_theme4 = weighted.mean(RPL_THEME4, w = AREA_SQMI)) %>% 
-  rename(GEOID = ZCTA) %>% 
-  mutate(GEOID = paste(GEOID)) %>% 
+    cdc_RPL_themes = mean(RPL_THEMES), 
+    cdc_RPL_theme1 = mean(RPL_THEME1),
+    cdc_RPL_theme2 = mean(RPL_THEME2),
+    cdc_RPL_theme3 = mean(RPL_THEME3),
+    cdc_RPL_theme4 = mean(RPL_THEME4)) %>% 
+  mutate(ZCTA = paste(ZCTA)) %>% 
   ungroup()
 
-dfa <- zzsvi_pa_2020 %>% 
+
+cdc2_result2 <- cdc2 %>% 
   left_join(
-   x <- result2020 %>% 
+     result2020 %>% 
       select(
-        GEOID, 
+        ZCTA = GEOID, 
         RPL_themes,
         RPL_theme1,
         RPL_theme2,
         RPL_theme3, 
         RPL_theme4
-      ))
+      )) %>% 
+  mutate(cor_all = cor(cdc_RPL_themes, RPL_themes),
+    cor1 = cor(cdc_RPL_theme1, RPL_theme1),
+    cor2 = cor(cdc_RPL_theme2, RPL_theme2),
+    cor3 = cor(cdc_RPL_theme3, RPL_theme3),
+    cor4 = cor(cdc_RPL_theme4, RPL_theme4))
 
-skim(dfa)
